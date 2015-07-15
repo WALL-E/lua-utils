6584561aa121b2f1792b593494b62a6bb2971a8a
张峥 <18601254750@163.com>
2015-07-15 21:50:22 +0800

    增加缩进参数

--- serialize.lua
+++ serialize.lua
@@ -1,5 +1,5 @@
 
-function serialize(o, indent)
+function serialize(o, depth, indent)
     if type(o) == "number" then
         io.write(o)
     elseif type(o) == "string" then
@@ -7,11 +7,11 @@ function serialize(o, indent)
     elseif type(o) == "table" then
         io.write("{\n")
         for k,v in pairs(o) do
-            io.write(string.rep(" ", indent+4), k, " = ")
-            serialize(v, indent+4)
+            io.write(string.rep(" ", indent*(depth+1)), k, " = ")
+            serialize(v, depth+1, indent)
             io.write(",\n")
         end
-        io.write(string.rep(" ", indent), "}")
+        io.write(string.rep(" ", indent*depth), "}")
     else
         error("cannot serialize a " .. type(o))
     end
@@ -19,7 +19,7 @@ end
 
 --test--
 io.write(string.rep("-", 30), "\n")
-serialize({abc=10, bcdfe=10} , 0)
+serialize({abc=10, bcdfe=10} , 0, 4)
 io.write("\n", string.rep("-", 30), "\n")
-serialize({a=10, bc=10, cdf={mno=10,no=20, xyz={y=100}}}, 0)
+serialize({a=10, bc=10, cdf={mno=10,no=20, xyz={y=100}}}, 0, 4)
 io.write("\n", string.rep("-", 30), "\n")
